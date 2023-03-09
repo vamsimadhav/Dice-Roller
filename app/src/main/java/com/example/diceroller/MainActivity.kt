19afc4b9354950c5152679b57b4f50bfee1b1ca7
vamsimadhav <vamsimadhav2000@gmail.com>
2023-03-09 15:40:44 +0530

    Added Dice Image and removed Text

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,11 +17,24 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.rollButton)
         rollButton.text = "Lets Roll"
 
-        val diceNumber: TextView = findViewById(R.id.diceNumber)
-        diceNumber.text = "099"
-
         rollButton.setOnClickListener {
-            Toast.makeText(this,"Button Clicked",Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val diceImage: ImageView = findViewById(R.id.diceImage)
+
+        val diceResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(diceResource)
+
     }
 }
