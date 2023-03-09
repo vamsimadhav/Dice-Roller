7ef56ff5c001143b8176a88be28a8ca27d9ff48f
vamsimadhav <vamsimadhav2000@gmail.com>
2023-03-09 15:29:02 +0530

    Added a toast after button is clicked

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.rollButton)
+        rollButton.text = "Lets Roll"
+
+        val diceNumber: TextView = findViewById(R.id.diceNumber)
+        diceNumber.text = "099"
+
+        rollButton.setOnClickListener {
+            Toast.makeText(this,"Button Clicked",Toast.LENGTH_SHORT).show()
+        }
     }
 }
